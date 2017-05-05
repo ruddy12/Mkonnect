f2b77e46c472e21c827df47f8997496debcd5567
ruddy12 <rufusngash@gmail.com>
2017-05-05 13:10:25 +0300

    sms api removed

--- img/sms/api/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$username = 'root';
-$password = 'dr67hj1';
-$host = 'localhost';
-$myDB = 'sdp';
-$link = mysql_connect($host,$username,$password);
-$conn = mysql_select_db($myDB, $link) or die('Could not open database $myDB');
-?>
--- img/sms/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-$msg = "You have successfully logged out";
-$link = 'index.php';
-header( 'Location: '.$link );
-?> 
